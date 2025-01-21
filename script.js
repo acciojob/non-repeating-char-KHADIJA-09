399a07ce79992bdae398a781d12bdaa854ff8751
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-21 11:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+   for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Loop through the string again to find the first character that appears only once
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+
+  // If no non-repeated character is found, return null
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
